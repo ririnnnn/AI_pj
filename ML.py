8880a0c04b43035c8e09e9450442bcfa1ca00db0
Quang <121388970+ririnnnn@users.noreply.github.com>
2022-12-27 13:41:19 +0700

    Add files via upload

--- ML.py
+++ ML.py
@@ -17,14 +17,11 @@ def load_dataset():
     return trainX, trainY, testX, testY
 
 # define cnn model
-def define_model(layer_conf):
+def define_model():
 	model = keras.models.Sequential()
-	model.add(keras.layers.Conv2D(layer_conf[0], (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
-	print("added layer ", layer_conf[0])
+	model.add(keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
 	model.add(keras.layers.MaxPooling2D((2, 2)))
-	for layer in layer_conf[1:]:
-		model.add(keras.layers.Conv2D(layer, (3, 3), activation='relu', kernel_initializer='he_uniform'))
-		print("added layer ", layer)
+	model.add(keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
 	model.add(keras.layers.MaxPooling2D((2, 2)))
 	model.add(keras.layers.Flatten())
 	model.add(keras.layers.Dense(100, activation='relu', kernel_initializer='he_uniform'))
@@ -32,23 +29,29 @@ def define_model(layer_conf):
 	opt = keras.optimizers.SGD(learning_rate=0.01, momentum=0.9)
 	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 	return model
-
-#train
-def train_model(dataX, dataY, n_folds, batch_sz, model):
-	kfold = KFold(n_folds, shuffle=True, random_state=1)
-	for train_ix, test_ix in kfold.split(dataX):
-		trainX, trainY, testX, testY = dataX[train_ix], dataY[train_ix], dataX[test_ix], dataY[test_ix]
-		model.fit(trainX, trainY, epochs=10, batch_size=batch_sz, validation_data=(testX, testY), verbose=0)
-		_, acc = model.evaluate(testX, testY, verbose=0)
-		print('> %.3f' % (acc * 100.0))
-	model.save("trainedmodel")
+ 
+#train model
+def train(trainX, trainY, testX, testY, batch_sz, model, model_path, diag_path):
+	history = model.fit(trainX, trainY, epochs=10, batch_size=batch_sz, validation_data=(testX, testY), verbose=0)
+	#evaluate on test dataset
+	_, acc = model.evaluate(testX, testY, verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	#summarize diagnostic
+	# plot loss
+	plt.subplot(2, 1, 1)
+	plt.title('Cross Entropy Loss')
+	plt.plot(history.history['loss'], color='blue', label='train')
+	plt.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	plt.subplot(2, 1, 2)
+	plt.title('Classification Accuracy')
+	plt.plot(history.history['accuracy'], color='blue', label='train')
+	plt.plot(history.history['val_accuracy'], color='orange', label='test')
+	# save diagnostics & model
+	plt.savefig(diag_path)
+	model.save(model_path)
 
 #main program
 trainX, trainY, testX, testY = load_dataset()
-layers = [64, 64]
-model = define_model(layers)
-#train_model(trainX, trainY, 5, 32, model)
-model.fit(trainX, trainY, epochs=10, batch_size=64, validation_data=(testX, testY), verbose=0)
-_, acc = model.evaluate(testX, testY, verbose=0)
-print('> %.3f' % (acc * 100.0))
-model.save("trainedmodel2")
+model = define_model()
+train(trainX, trainY, testX, testY, 64, model, "trained_model" , "diag.png")
